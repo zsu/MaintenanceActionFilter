d751a009f7f001fcc03263d110ae03369de72bba
zsu <azure.ss@gmail.com>
2017-01-14 14:25:36 -0500

    add missing files

---
+++ MaintenanceActionFilter/MaintenanceSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceActionFilter
+{
+	public interface IMaintenanceSettingProvider
+	{
+		DateTime StartTimeUtc { get; }
+		DateTime EndTimeUtc { get; }
+		string MaintenanceWarningMessage { get; }
+		double WarningLeadTime { get; }
+		bool CanByPass(IPrincipal user);
+		string GetMaintenanceUrl(Uri requestUrl);
+	}
+}
